5bbab3477258cdc35f43619fbb85044d8ff27299
Laksh Nanwani <lakshanshul@gmail.com>
2022-10-15 16:01:00 +0530

    minor changes

--- workspace/src/kangaroo_x2_driver/src/teleop_cmd_vel.py
+++ workspace/src/kangaroo_x2_driver/src/teleop_cmd_vel.py
@@ -36,10 +36,10 @@ else:
   import tty, termios
 
 WHEELCHAIR_MAX_LIN_VEL = 2 		#Max velocity for Wheelchair_V(m/s)
-WHEELCHAIR_MAX_ANG_VEL = 0.5
+WHEELCHAIR_MAX_ANG_VEL = 2
 
 LIN_VEL_STEP_SIZE = 0.005 #0.1 m/s
-ANG_VEL_STEP_SIZE = 0.15 #0.1 m/s
+ANG_VEL_STEP_SIZE = 0.25 #0.1 m/s
 
 msg = """
 Control Your Wheelchair!
