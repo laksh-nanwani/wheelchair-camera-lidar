521caf93807e402fe63abd1ceb9ac906dfdc0445
Raghav Prabhakar <raghavprabhakar66@gmail.com>
2023-02-05 19:52:37 +0530

    add new map

--- workspace/src/language_nav/src/talker.py
+++ workspace/src/language_nav/src/talker.py
@@ -2,20 +2,23 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import String
+import os
 
-def talker():
+def talker(filepath):
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        f = open("/home/raghav/Dev/wheelchair-camera-lidar/commands.txt", "r")
+        f = open(filepath, "r")
         hello_str = f.read() 
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
 
 if __name__ == '__main__':
+    path = os.path.join("/".join(__file__.split("/")[:6]), "commands.txt")
+    
     try:
-        talker()
+        talker(path)
     except rospy.ROSInterruptException:
         pass
